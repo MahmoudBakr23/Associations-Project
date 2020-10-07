1d5db5a170becfa12d438370495fb1f4d815d833
Vitalie Melnic <vmwhoami@gmail.com>
2020-10-07 17:27:37 +0300

    destroy migration Invitations

--- db/migrate/20201007142057_create_table_invitations.rb
+++
@@ -1,8 +0,0 @@
-class CreateTableInvitations < ActiveRecord::Migration[5.2]
-  def change
-    create_table :invitations do |t|
-      t.references :user, foreign_key: true
-      t.references :event, foreign_key: true
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_07_142057) do
+ActiveRecord::Schema.define(version: 2020_10_07_141034) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -21,13 +21,6 @@ ActiveRecord::Schema.define(version: 2020_10_07_142057) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_events_on_user_id"
-  end
-
-  create_table "invitations", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "event_id"
-    t.index ["event_id"], name: "index_invitations_on_event_id"
-    t.index ["user_id"], name: "index_invitations_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
