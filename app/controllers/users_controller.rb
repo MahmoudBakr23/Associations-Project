f2dc1ccdf570b14e7ec24b440ef25b9db3cb5f52
Mahmoud Bakr <mbakr6821@gmail.com>
2020-10-07 14:50:05 +0200

    modify create method and the routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
   end
 
   private
-  
+
   def user_params
     params.require(:user).permit(:name)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
-  get 'users/show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: %i[new create show]
+  root 'users#new'
 end
