0b9753f7f94bde8cd2121830e40819ab311b9b4f
Vitalie Melnic <vmwhoami@gmail.com>
2020-10-07 17:06:54 +0300

    Add session login logout functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,21 @@
 class SessionsController < ApplicationController
+ include SessionsHelper
 
-    def new
-        
+    def new   
     end
 
     def create
-        
+      user = User.find_by(name: params[:sessions][:name])
+      if user
+        log_in(user)
+        redirect_to user_path(user)
+      else
+        render "new"
+      end
     end
 
     def destroy
-        
+        @current_user = nil
+        session[:user_id] = nil
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: %i[new create show]
-  resources :sessions, only: %i[new create destroy]
+  get '/login', to: 'sessions#new'
+  post '/login',to: 'sessions#create'
+  delete '/logout',to: 'sessions#destroy'
   root 'users#new'
 end
