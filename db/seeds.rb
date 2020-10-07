f310aed3fbd508b6f1d480d19bf8373f4e0fb3f7
Vitalie Melnic <vmwhoami@gmail.com>
2020-10-07 16:12:35 +0300

    Add seeded Users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create!(name:"Vitalie")
+User.create!(name:"Mahmoud")
+User.create!(name:"Ariel")
+User.create!(name:"Olea")
+User.create!(name:"Adam")
+
+puts "Users created"
