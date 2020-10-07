473cee83faf1bffbbbc6449f5843dc8e41706d90
Mahmoud Bakr <mbakr6821@gmail.com>
2020-10-07 14:56:20 +0200

    generate sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+
+    def new
+        
+    end
+
+    def create
+        
+    end
+
+    def destroy
+        
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :users, only: %i[new create show]
+  resources :sessions, only: %i[new create destroy]
   root 'users#new'
 end
