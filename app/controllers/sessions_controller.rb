abbfd98fdf6535d5ee248b2dd03c07771e5adbda
Mahmoud Bakr <mbakr6821@gmail.com>
2020-10-08 17:07:44 +0200

    get the logic out of the views

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,5 +17,6 @@ class SessionsController < ApplicationController
     def destroy
         @current_user = nil
         session[:user_id] = nil
+        redirect_to root_path
     end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,12 +1,19 @@
 module SessionsHelper
   
-def log_in(user)
-  session[:user_id] = user.id
-end
+  def links
+     if current_user 
+       link_to  "Logout", logout_path, method: :delete  
+     else 
+       link_to  "Login", login_path unless current_user 
+   end 
+  end
 
-def current_user
-  @current_user ||= User.find_by(id: session[:user_id])
-end
+  def log_in(user)
+    session[:user_id] = user.id
+  end
 
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
 
 end
