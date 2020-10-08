6b6222f19aad18f5621f3c63b3ea01783b85c3f5
Mahmoud Bakr <mbakr6821@gmail.com>
2020-10-08 22:49:15 +0200

    modify event.rb model file

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
 
   has_many :invitations, foreign_key: 'attended_event_id'
-  has_many :attendees, through: :invitations, source: :attendee, foreign_key: 'attendee_id'
+  has_many :attendees, through: :invitations, source: :attendee
 
   scope :past, -> { where(['date < ?', Time.now]) }
   scope :future, -> { where(['date > ?', Time.now]) }
